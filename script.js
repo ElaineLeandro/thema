39743b20bcbee207f7adf66c0aba577d6888eb11
Elaine Leandro <elainerleandro1@gmail.com>
2023-03-01 13:30:34 -0300

    Ajuste no função  darkModeCard

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 
-function darkMode() {
-  const body = document.querySelector('body')
-  const header = body.querySelector('.header')
-  body.classList.toggle('dark')
-  header.classList.toggle('dark-title')
-  
+function darkModeCard() {
+  const cardItem = document.querySelectorAll('.item')
+  cardItem.forEach((card) => card.classList.toggle('dark-item'))
 }
 
-function darkModeContainer() {
-  const cardItem = [...document.querySelectorAll('.item')]
-  cardItem.map((card) => {card.classList.toggle('dark-item')})
-  console.log("Cor",cardItem)
-  
+function darkMode() {
+  const body = document.querySelector('body')
+  const header = document.querySelector('.header')
+  const text = document.querySelectorAll('div p, h2')
+  body.classList.toggle('dark')
+  header.classList.toggle('dark-title')
+  text.forEach((contex) => contex.classList.toggle('content')) 
+
+  darkModeCard()
 }
 
 document.querySelector('.button').addEventListener("click", darkMode)
-document.querySelector('.button').addEventListener("click", darkModeContainer)
+
 
